b590d0ce12efc8b7b346ffb88d753aafb7bede06
glqdlt <dlfdnd0725@gmail.com>
2021-05-10 22:00:19 +0900

    pr

--- src/main/java/com/glqdlt/ex/prtest/PrTestApplication.java
+++ src/main/java/com/glqdlt/ex/prtest/PrTestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrTestApplication {
 
+//    i pr request
+
     public static void main(String[] args) {
         SpringApplication.run(PrTestApplication.class, args);
     }
