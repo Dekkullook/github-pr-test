9959619f8f5d8570a15303020e9f81c02955dd89
glqdlt <dlfdnd0725@gmail.com>
2021-05-10 22:02:32 +0900

    4 commit

--- src/main/java/com/glqdlt/ex/prtest/PrTestApplication.java
+++ src/main/java/com/glqdlt/ex/prtest/PrTestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//    4 commit
 public class PrTestApplication {
 
 //    i pr request
