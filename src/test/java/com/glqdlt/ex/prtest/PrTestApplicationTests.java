c5b41bcd6838e04a59057978f802c1aba7c0849b
glqdlt <dlfdnd0725@gmail.com>
2021-05-10 21:59:37 +0900

    commit

---
+++ src/test/java/com/glqdlt/ex/prtest/PrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.prtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
